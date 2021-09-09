f426c4d45fbdb4d6ce941e48bc589d93f3adbaf4
veiuper <valeriyfinans@gmail.com>
2021-09-09 10:39:50 +0500

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/User.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker;
+
+public class User {
+    private final int id;
+    private final String username;
+    private final String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", username='" + username + '\''
+                + ", password='" + password + '\''
+                + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/UserStory.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class UserStory {
+    public static void main(String[] args) {
+        User user = new User(1, "root", "root");
+        System.out.println(user);
+    }
+}
