a40ee68073c5fa22a7f786844c31a2bdc13c33ad
veiuper <valeriyfinans@gmail.com>
2021-09-11 11:47:26 +0500

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,6 +2,74 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("==== Create a new item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавлена заявка " + item);
+    }
+
+    public static void showAllItems(Tracker tracker) {
+        System.out.println("==== Show all items ====");
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item curItem : items) {
+                System.out.println(curItem);
+            }
+        } else {
+            System.out.println("Хранилище не содержит заявок");
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("==== Edit item ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        boolean rsl = tracker.replace(id, item);
+        if (rsl) {
+            System.out.println("Заявка успешно изменена");
+        } else {
+            System.out.println("Не удалось изменить заявку");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("==== Delete item ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        boolean rsl = tracker.delete(id);
+        if (rsl) {
+            System.out.println("Заявка удалена успешно");
+        } else {
+            System.out.println("Ошибка удаления заявки");
+        }
+    }
+
+    public static void findItemById(Input input, Tracker tracker) {
+        System.out.println("==== Find item by id ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Заявка с введенным id '" + id + "' не найдена");
+        }
+    }
+
+    public static void findItemsByName(Input input, Tracker tracker) {
+        System.out.println("==== Find items by name ====");
+        String name = input.askStr("Enter name: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item curItem : items) {
+                System.out.println(curItem);
+            }
+        } else {
+            System.out.println("Заявки с именем '" + name + "' не найдены");
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
@@ -10,61 +78,17 @@ public class StartUI {
             if (select == 6) {
                 run = false;
             } else if (select == 0) {
-                System.out.println("==== Create a new item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
-                System.out.println("Добавлена заявка " + item);
+                createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("==== Show all items ====");
-                Item[] items = tracker.findAll();
-                if (items.length > 0) {
-                    for (Item curItem : items) {
-                        System.out.println(curItem);
-                    }
-                } else {
-                    System.out.println("Хранилище не содержит заявок");
-                }
+                showAllItems(tracker);
             } else if (select == 2) {
-                System.out.println("==== Edit item ====");
-                int id = Integer.parseInt(input.askStr("Enter id: "));
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                boolean rsl = tracker.replace(id, item);
-                if (rsl) {
-                    System.out.println("Заявка успешно изменена");
-                } else {
-                    System.out.println("Не удалось изменить заявку");
-                }
+                editItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("==== Delete item ====");
-                int id = Integer.parseInt(input.askStr("Enter id: "));
-                boolean rsl = tracker.delete(id);
-                if (rsl) {
-                    System.out.println("Заявка удалена успешно");
-                } else {
-                    System.out.println("Ошибка удаления заявки");
-                }
+                deleteItem(input, tracker);
             } else if (select == 4) {
-                System.out.println("==== Find item by id ====");
-                int id = Integer.parseInt(input.askStr("Enter id: "));
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item);
-                } else {
-                    System.out.println("Заявка с введенным id '" + id + "' не найдена");
-                }
+                findItemById(input, tracker);
             } else if (select == 5) {
-                System.out.println("==== Find items by name ====");
-                String name = input.askStr("Enter name: ");
-                Item[] items = tracker.findByName(name);
-                if (items.length > 0) {
-                    for (Item curItem : items) {
-                        System.out.println(curItem);
-                    }
-                } else {
-                    System.out.println("Заявки с именем '" + name + "' не найдены");
-                }
+                findItemsByName(input, tracker);
             } else {
                 System.out.println("Пользователь выбрал: " + select);
             }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -36,7 +36,7 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        boolean rsl = index != 1;
+        boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
             items[index] = item;
