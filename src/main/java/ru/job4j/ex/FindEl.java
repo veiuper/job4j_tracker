7009e76cff8148ace33b3bd35c58f0d5c4ada7d2
veiuper <valeriyfinans@gmail.com>
2021-09-15 01:06:43 +0500

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,25 +1,38 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws FindNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
+        for (int i = 0; i < values.length; i++) {
+            if (key.equals(values[i])) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
-            throw new FindNotFoundException("The index for the string '" + key + "' was not found");
+            throw new ElementNotFoundException("The index for the string '" + key + "' was not found");
         }
         return rsl;
     }
 
-    public static void main(String[] args) {
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String curString : abuses) {
+            if (value.equals(curString)) {
+                throw new ElementAbuseException("The string '" + value + "' contains a forbidden key");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
         try {
-            indexOf(new String[]{"a", "b"}, "a");
-            indexOf(new String[]{"a", "b"}, "c");
-        } catch (FindNotFoundException e) {
-            e.printStackTrace();
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException enf) {
+            enf.printStackTrace();
         }
     }
 }
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,36 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User curUser : users) {
+            if (login.equals(curUser.getUsername())) {
+                return curUser;
+            }
+        }
+        throw new UserNotFoundException("The user '" + login + "' was not found");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("The user '" + user.getUsername() + "' did not pass verification");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Valeriy Osipov", true)
+        };
+        try {
+            User user = findUser(users, "Valeriy Osipov");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ui) {
+            ui.printStackTrace();
+        } catch (UserNotFoundException unf) {
+            unf.printStackTrace();
+        }
+    }
+}
