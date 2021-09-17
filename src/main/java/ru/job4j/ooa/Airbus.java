e0658aca3b1eb35fd994256fbda452632218144e
veiuper <valeriyfinans@gmail.com>
2021-09-17 16:24:26 +0500

    4. final [#334073 #204095]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -21,7 +21,8 @@ public final class Airbus {
     }
 
     public void printCountEngine() {
-        System.out.println("Количество двигателей равно: " + (("A380".equals(name)) ? 4 : COUNT_ENGINE));
+        int count = ("A380".equals(name)) ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + count);
     }
 
     @Override
