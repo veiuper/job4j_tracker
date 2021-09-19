973eb9c1f38526308285a124bc52b79297ee92f4
veiuper <valeriyfinans@gmail.com>
2021-09-19 15:37:42 +0500

    5. Конвертация листа массивов в один лист Integer [#10037]

---
+++ src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertListTest {
+
+    @Test
+    public void whenTwoList() {
+        List<int[]> in = new ArrayList<>();
+        in.add(new int[]{1});
+        in.add(new int[]{2, 3});
+        List<Integer> actual = ConvertList.convert(in);
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenEmptyListArraysThenEmptyList() {
+        List<int[]> in = new ArrayList<>();
+        in.add(new int[]{});
+        in.add(new int[]{});
+        List<Integer> actual = ConvertList.convert(in);
+        List<Integer> expected = new ArrayList<>();
+        assertThat(actual, is(expected));
+    }
+}
