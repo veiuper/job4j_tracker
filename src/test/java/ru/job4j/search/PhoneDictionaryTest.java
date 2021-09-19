f9368d3c32e1adbefb184ecf07eb93fcbe8bec5b
veiuper <valeriyfinans@gmail.com>
2021-09-19 13:53:47 +0500

    1. Телефонный справочник на базе ArrayList [#41598 #205124]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,12 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Valeriy");
         assertThat(persons.get(0).getSurname(), is("Osipov"));
     }
+
+    @Test
+    public void whenFindByNameEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Valeriy", "Osipov", "+7999999999", "Sredneuralsk"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.size(), is(0));
+    }
 }
