0a59abfa46fc637ab4c9c67d63fcfc7356db16f3
veiuper <valeriyfinans@gmail.com>
2021-09-27 02:33:53 +0500

    5. Подсчет функции в диапазоне. [#24251 #208881]

--- src/main/java/ru/job4j/function/FunctionValuesInRange.java
+++ src/main/java/ru/job4j/function/FunctionValuesInRange.java
@@ -8,10 +8,9 @@ import java.util.function.Function;
 public class FunctionValuesInRange {
     public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> rsl = new ArrayList<>(end - start);
-        Function<Integer, Double> function = (i) -> func.apply(Double.valueOf(i));
         Consumer<Double> consumerAdd = rsl::add;
         for (int i = start; i < end; i++) {
-            consumerAdd.accept(function.apply(i));
+            consumerAdd.accept(func.apply((double) i));
         }
         return rsl;
     }
